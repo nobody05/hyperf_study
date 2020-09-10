936aed69b4b0d678b4f0fab7f891ca7434805074
gaozhi <gaozhi@weiboyi.com>
2020-09-10 09:59:35 +0800

    test

---
+++ web/build/precache-manifest.c8c9aaed9f4387fefad2a4f7cac60313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a01b7cf7c31422de372440a35eda5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41dbd50ac207e467a9b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41dbd50ac207e467a9b0",
+    "url": "/static/js/main.c367d311.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
